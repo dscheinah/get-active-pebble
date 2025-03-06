eb5817793bd1819aae3a9ac0748a62be5ca97f92
dscheinah <github@jb.chmann.net>
2025-03-06 20:51:55 +0100

    add accessibility checks to step averages

--- app/src/c/health/health.c
+++ app/src/c/health/health.c
@@ -7,8 +7,21 @@ void health_init(State* state) {
   time_t end = start + SECONDS_PER_DAY;
 
   state->health->steps = health_service_sum_today(HealthMetricStepCount);
-  state->health->steps_week = health_service_sum_averaged(HealthMetricStepCount, start, end, HealthServiceTimeScopeDaily);
-  state->health->steps_todo = health_service_sum_averaged(HealthMetricStepCount, now, end, HealthServiceTimeScopeDaily);
+
+  HealthServiceAccessibilityMask mask;
+
+  mask = health_service_metric_averaged_accessible(HealthMetricStepCount, start, end, HealthServiceTimeScopeDaily);
+  if (mask & HealthServiceAccessibilityMaskAvailable) {
+    state->health->steps_week = health_service_sum_averaged(HealthMetricStepCount, start, end, HealthServiceTimeScopeDaily);
+  } else {
+    state->health->steps_week = state->settings->step_target;
+  }
+  mask = health_service_metric_averaged_accessible(HealthMetricStepCount, now, end, HealthServiceTimeScopeDaily);
+  if (mask & HealthServiceAccessibilityMaskAvailable) {
+    state->health->steps_todo = health_service_sum_averaged(HealthMetricStepCount, now, end, HealthServiceTimeScopeDaily);
+  } else {
+    state->health->steps_todo = state->health->steps / state->event->hours_done * (HOURS_PER_DAY - state->event->hours_done);
+  }
 
   state->health->active = health_service_sum_today(HealthMetricActiveSeconds);
   if (state->health_persistent->wakeup != state->wakeup) {
