a709f28f3e14603d8c644b4c50042813d4ca7564
dscheinah <github@jb.chmann.net>
2025-03-13 20:02:33 +0100

    stop active hours with settings end

--- app/src/c/event/event.c
+++ app/src/c/event/event.c
@@ -6,9 +6,18 @@ void event_init(State* state) {
 
   tm* next = localtime(&state->event->now);
 
+  int hours_done = 0;
+  if (next->tm_hour > state->settings->begin) {
+    if (next->tm_hour > state->settings->end && state->settings->end > state->settings->begin) {
+      hours_done = state->settings->end - state->settings->begin;
+    } else {
+      hours_done = next->tm_hour - state->settings->begin;
+    }
+  }
+
   state->event->day = next->tm_mday;
   state->event->hours = next->tm_hour;
-  state->event->hours_done = next->tm_hour > state->settings->begin ? next->tm_hour - state->settings->begin : 0;
+  state->event->hours_done = hours_done;
   state->event->muted_warnings = next->tm_hour < state->settings->begin || next->tm_hour > state->settings->end;
 
   next->tm_hour++;
