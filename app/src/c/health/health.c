312e3dc867a9be75d1c2d93554c49bc5d3ce3a0a
dscheinah <github@jb.chmann.net>
2025-03-15 12:34:21 +0100

    manually calculate sleep average

--- app/src/c/health/health.c
+++ app/src/c/health/health.c
@@ -2,9 +2,9 @@
 #include "health.h"
 
 void health_init(State* state) {
-  time_t now = state->event->now;
   time_t start = time_start_of_today();
   time_t end = start + SECONDS_PER_DAY;
+  time_t week = start - (6 * SECONDS_PER_DAY);
 
   state->health->steps = health_service_sum_today(HealthMetricStepCount);
 
@@ -35,9 +35,9 @@ void health_init(State* state) {
     state->health_persistent->active_current = state->health->active;
   }
 
-  mask = health_service_metric_averaged_accessible(HealthMetricSleepSeconds, start, end, HealthServiceTimeScopeDaily);
+  mask = health_service_metric_accessible(HealthMetricSleepSeconds, week, end);
   if (mask & HealthServiceAccessibilityMaskAvailable) {
-    state->health->sleep_week = health_service_sum_averaged(HealthMetricSleepSeconds, start, end, HealthServiceTimeScopeDaily);
+    state->health->sleep_week = health_service_sum(HealthMetricSleepSeconds, week, end) / 7;
   } else {
     state->health->sleep_week = 0;
   }
